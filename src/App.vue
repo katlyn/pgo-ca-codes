d3965a08c8a8c8f6af5cfaa1091b070b5814c64d
Katlyn Lorimer <katlyn@katlyn.dev>
2024-02-23 21:51:33 -0900

    feat: initial commit and project scaffoling

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const name = ref("Beep boosdfsdfsdfsdfsdfp!");
+</script>
+
+<template>
+  <main class="mx-auto max-w-prose">
+    <h1>Pokémon Go CA Codes</h1>
+  </main>
+</template>
